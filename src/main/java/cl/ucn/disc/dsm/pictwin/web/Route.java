f661755c15704be59776793fc87715e01703f831
Nicrono42 <86138895+Nicrono42@users.noreply.github.com>
2024-12-03 15:41:21 -0300

    backend | create Route class

---
+++ src/main/java/cl/ucn/disc/dsm/pictwin/web/Route.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2024. Departamento de Ingenieria de Sistemas y Computacion
+ */
+
+package cl.ucn.disc.dsm.pictwin.web;
+
+import lombok.Getter;
+import lombok.NonNull;
+
+import java.lang.reflect.Method;
+import java.util.logging.Handler;
+
+@Getter
+public abstract class Route {
+
+    // the method
+    protected Method method;
+
+    // the path
+    protected String path;
+
+    // the handler
+    protected Handler handler;
+
+    // the constructor
+    protected Route(@NonNull final Method method, @NonNull final String path) {
+        this.method = method;
+        this.path = path;
+    }
+
+    // methods
+    public enum method {
+        GET,
+        POST,
+        Put,
+    }
+}

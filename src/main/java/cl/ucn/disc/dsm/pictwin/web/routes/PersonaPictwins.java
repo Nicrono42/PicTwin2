226e556976d2278ca5d0e1a91765cecfc94111bd
Nicrono42 <86138895+Nicrono42@users.noreply.github.com>
2024-12-17 13:43:38 -0300

    feat: adds constructor and buildHandler function to the class PersonaPictwins.java

--- src/main/java/cl/ucn/disc/dsm/pictwin/web/routes/PersonaPictwins.java
+++ src/main/java/cl/ucn/disc/dsm/pictwin/web/routes/PersonaPictwins.java
@@ -1,4 +1,31 @@
 package cl.ucn.disc.dsm.pictwin.web.routes;
 
-public class PersonaPictwins {
+import cl.ucn.disc.dsm.pictwin.services.Controller;
+import cl.ucn.disc.dsm.pictwin.web.Route;
+
+import io.javalin.http.Handler;
+
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public final class PersonaPictwins extends Route {
+
+    /** The Contructor*/
+    public PersonaPictwins(@NonNull final Controller controller) {
+
+        super(Method.GET, "/api/personas/{ulid}/pictwins");
+
+        this.handler = buildHandler(controller);
+    }
+
+    /** Build the hnadler. */
+    private static Handler buildHandler(Controller controller) {
+        return ctx -> {
+            String ulid = ctx.pathParam("ulid");
+            log.debug("Detected ulid = {} for Persona.", ulid);
+
+            ctx.json(controller.getPicTwins(ulid));
+        };
+    }
 }
